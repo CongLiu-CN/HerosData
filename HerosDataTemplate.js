16f746468d728e18b56eee94f193f32e2b023a85
ikitty <xkeitty@163.com>
2015-04-22 13:11:18 +0800

    add data template

---
+++ HerosDataTemplate.js
@@ -0,0 +1,14 @@
+    ,{
+        "name": ""
+        ,"nick": ""
+        ,"star": ""
+        ,"pos": ""
+        ,"weapon": ""
+        ,"skill": ""
+        ,"skillExt": ""
+        ,"attack": "0"
+        ,"attackArea": "0"
+        ,"defense": "0"
+        ,"detail": ""
+        ,"order": "1"
+    }
